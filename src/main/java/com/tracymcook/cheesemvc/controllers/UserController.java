8bb7a597c6645fd37db45cef56980f5a9fd20f49
Tracy Cook <tracymcook23@gmail.com>
2019-06-13 20:30:40 -0500

    Displays user sign-up form.

--- src/main/java/com/tracymcook/cheesemvc/controllers/UserController.java
+++ src/main/java/com/tracymcook/cheesemvc/controllers/UserController.java
@@ -1,6 +1,31 @@
 package com.tracymcook.cheesemvc.controllers;
 
+import com.tracymcook.cheesemvc.models.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("user")
 public class UserController {
 
+    @RequestMapping("add")
+    public String add(Model model) {
+        return "user/add";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String add(Model model, @ModelAttribute User user, String verify) {
+
+       if(user.getPassword().equals(verify)) {
+           model.addAttribute("user", user);
+           return  "user/index";
+       }
+
+       return "user/add";
+
+    }
 
 }
